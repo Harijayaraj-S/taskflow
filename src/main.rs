961d604ee74cde780bc4b3b3320652a71b1075a8
Harijayaraj-S <harijayaraj011@gmail.com>
2025-12-17 07:38:33 +0530

    feat:  base file structure

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
+// Main
+
+use anyhow::Result;
+
+mod app;
+mod config;
+mod routes;
+
+#[tokio::main]
+async fn main() -> Result<()> {
     println!("Hello, taskflow!");
+
+    Ok(())
 }
---
+++ src/routes/mod.rs
@@ -0,0 +1,3 @@
+// Routes Mod
+
+mod health;
